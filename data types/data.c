fa1a0090e6f91a0b4136d08854b2446097481354
rajeshsaharan <rajeshsharan1607@gmail.com>
2022-11-06 08:19:13 +0530

    intro-to-programming

---
+++ data types/data.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+void main(){
+    int a ; // variable declaration
+    float b;
+    b = 3.14;
+    char c ;
+    c = 'd';
+    a= 5; //variable intilization
+    printf("%d", a); // passing to printf function
+    printf("%c", c);
+}
